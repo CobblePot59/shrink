6b314bd7768934465ddfa36e90752db01405781e
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2023-09-26 20:14:08 +0200

    Update app.py

--- volumes/app/app.py
+++ volumes/app/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_toastr import Toastr
 from hashids import Hashids
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+
+toastr = Toastr(app)
 
 hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
